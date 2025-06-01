a864a49870208d6243b473d863db5416b4bbb7d0
Ignacio Esquivel <jose.i.audeves@gmail.com>
2025-06-01 14:56:40 -0700

    Cambiado todo a typescript

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import {
+  getAllTasks,
+  createTask,
+  markTaskDone,
+  deleteTask
+} from './taskController';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/tasks', getAllTasks);
+app.post('/tasks', createTask);
+app.put('/tasks/:id', markTaskDone); 
+app.delete('/tasks/:id', deleteTask);
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
